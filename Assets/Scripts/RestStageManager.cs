77c46f0641f0a2c62577e16ed7b0e99c6b3e2766
0nyouhyen <zxfy12345@gmail.com>
2026-08-12 21:51:41 +0900

    connect
    1스테이지와 휴식 스테이지의 골드 연동

--- Assets/Scripts/RestStageManager.cs
+++ Assets/Scripts/RestStageManager.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class RestStageManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("UI 연동")]
+    public TMP_Text goldText; // 휴식 스테이지의 골드 표시 텍스트 UI
+
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // GameManager가 존재하는지 확인 후, 싱글톤에 남아있는 gold 값을 가져와서 표시
+        if (GameManager.Instance != null && goldText != null)
+        {
+            goldText.text = GameManager.Instance.gold.ToString();
+            Debug.Log($"[RestStage] 현재 골드 연동 완료: {GameManager.Instance.gold}G");
+        }
     }
 }
